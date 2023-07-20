16d20484b8f124f5fbc30e469cd53e3bd6bc37cd
Jonathan Bijl <sannoayo@gmail.com>
2023-07-20 16:07:35 -0400

    Navbar.js updated

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom"
+import Navbar from "./components/Navbar";
+import HomePage from "./pages/HomePage";
  
 function App() {
   return (
-    <div className="App"></div>
+    <div className="App">
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element ={ <HomePage /> } />
+      </Routes>
+    </div>
   );
 }
 export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import axios from "axios";
+
+export default function Navbar() {
+  return (
+    <div>
+        <NavLink to ="/">Home</NavLink>
+    </div>
+  );
+}
